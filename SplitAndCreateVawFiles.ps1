466fa29c15954d139b1b3cb3ce340a65395f8b97
aadennis <dennis.circ@gmail.com>
2016-02-22 20:50:10 +0000

    Last chunk was writing text,  but not speaking. Fixed
    Last chunk was writing text,  but not speaking. Fixed

--- SplitAndCreateVawFiles.ps1
+++ SplitAndCreateVawFiles.ps1
@@ -2,12 +2,22 @@
     "{0}{1}_{2}.{3}" -f ($outputRootDir, $outputFileNamePrefix, $chunk, $extension)
 }
 
+function Write-WavFile() {
+    $speech = New-Object -TypeName System.Speech.Synthesis.SpeechSynthesizer
+    $speech.SelectVoice("Microsoft Hazel Desktop")
+    $textToSpeak = Get-Content -Path $(Get-FileName) -Encoding UTF8
+    $speech.SetOutputToWaveFile($(Get-FileName "wav"))
+    $speech.Speak($textToSpeak)
+    $speech.Dispose()
+    $speech = $null
+}
+
 # This expects a $splitMarker in a source file, to denote the string where 1 file is to end and another is to start
 # All the output files have the same name, differing only by the incrementing counter $chunk
 function Split-File (
     $fileToSplit = 'C:\Temp\PandP.txt',
     $splitMarker = "SPLITHERE",
-    $outputFileNamePrefix = "proze",
+    $outputFileNamePrefix = "austenx",
     $outputRootDir = "c:\temp\"
     ) {
     Add-Type -AssemblyName System.Speech
@@ -19,19 +29,13 @@ function Split-File (
     $speech.SelectVoice("Microsoft Hazel Desktop")
     while (($line = $reader.ReadLine()) -ne $null) {
         if ($line -match $splitMarker) {
-            $speech = New-Object -TypeName System.Speech.Synthesis.SpeechSynthesizer
-            $speech.SelectVoice("Microsoft Hazel Desktop")
-            $textToSpeak = Get-Content -Path $(Get-FileName) -Encoding UTF8
-            $speech.SetOutputToWaveFile($(Get-FileName "wav"))
-            $speech.Speak($textToSpeak)
-            $speech.Dispose()
-            $speech = $null
+            Write-WavFile
             $chunk++
         } else {
             Add-Content -Path $(Get-FileName "txt") -Value $line -Encoding utf8
         }
     }
-
+    Write-WavFile
     $reader.Close()
     $reader.Dispose()
     $reader = $null
