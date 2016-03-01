ad35618c13720ba6340036dc4fe694c73d408800
aadennis <dennis.circ@gmail.com>
2016-03-01 21:08:40 +0000

    Supports with and without split markers
    Supports with and without split markers

--- SplitAndCreateVawFiles.ps1
+++ SplitAndCreateVawFiles.ps1
@@ -11,35 +11,92 @@ function Write-WavFile($speechChunk) {
     $speech = $null
 }
 
+function Check-Input (
+    $splitMarker,
+    $numberOfLinesPerFile
+) {
+    Write-Host "Validating input..."
+    $splitMarker
+    $numberOfLinesPerFile
+    if (($splitMarker -ne $null) -and ($numberOfLinesPerFile -ne $null)) {
+        Write-Host "You cannot specify both a SplitMarker and the required lines per file. Exiting..."
+        exit
+    }
+
+    
+}
+
+function Write-WavFilex() {
+     $speech = New-Object -TypeName System.Speech.Synthesis.SpeechSynthesizer
+     $speech.SelectVoice("Microsoft Hazel Desktop")
+     $textToSpeak = Get-Content -Path $(Get-FileName) -Encoding UTF8
+     $speech.SetOutputToWaveFile($(Get-FileName "wav"))
+     $speech.Speak($textToSpeak)
+     $speech.Dispose()
+     $speech = $null
+ }
+
+function Check-StringInFile (
+    $fileName,
+    $stringToFind
+) {
+    $totalLinesFound = $(Get-Content $fileName) -match $stringToFind
+    if ($totalLinesFound -eq 0) {
+        return $false
+    }
+    return $true
+}
+
+
 function Split-File (
     $fileToSplit = 'C:\Temp\pandp.txt',
     $splitMarker = $null,
     $outputFileNamePrefix = "\Judex",
     $outputRootDir = "c:\temp\",
-    $numberOfLinesPerFile = 4
+    $numberOfLinesPerFile = 100
 ) {
+    Check-Input $splitMarker $numberOfLinesPerFile
+    if (($splitMarker -ne $null) -and ((Check-StringInFile $fileToSplit $splitMarker)) -eq $false) {
+        $stuff = "Could not find split marker [{0}] in file [{1}]. Exiting..." -f $splitMarker, $fileToSplit
+        Write-Host $stuff 
+        exit
+    }
+
     Add-Type -AssemblyName System.Speech
     $reader = New-Object -TypeName System.IO.StreamReader($fileToSplit, [System.Text.Encoding]::UTF8)
-    $chunk = $numberOfLinesPerFile
-    $currFileCount = 1
-    $currLineCount = 0
     $speech = New-Object -TypeName System.Speech.Synthesis.SpeechSynthesizer
     $speech.SelectVoice("Microsoft Hazel Desktop")
+
+    $currFileCount = 1
+    $currLineCount = 0
+
     $currChunk = @()
     $currChunk += "Start of book - section {0};" -f ($currFileCount)
     
     while (($line = $reader.ReadLine()) -ne $null) {
-        if ($currLineCount++ -ge $numberOfLinesPerFile) {
-            $currChunk += "End of section {0}" -f ($currFileCount)
-            Write-WavFile $currChunk
-            $currLineCount = 0
-            $currFileCount++
-            $currChunk = @()
-            $currChunk += "Start of Section {0};" -f ($currFileCount)
-            "{0}:{1}" -f ($(Get-Date), $($currChunk))
-        } else {
-            $currChunk += $line
+
+        # For now, just separate the 2 approaches into different code blocks...
+        if ($splitMarker -eq $null) {
+            if ($currLineCount++ -ge $numberOfLinesPerFile) {
+                $currChunk += "End of section {0}" -f ($currFileCount)
+                Write-WavFile $currChunk
+                $currLineCount = 0
+                $currFileCount++
+                $currChunk = @()
+                $currChunk += "Start of Section {0};" -f ($currFileCount)
+                "{0}:{1}" -f ($(Get-Date), $($currChunk))
+            } else {
+                $currChunk += $line
+            }
+        } else { # the user is specifying the file break...
+            if ($line -match $splitMarker) {
+               Write-WavFilex
+               $currFileCount++
+            } else {
+                Add-Content -Path $(Get-FileName "txt") -Value $line -Encoding UTF8
+            }
         }
+
     }
     Write-WavFile $currChunk
     $reader.Close()
@@ -49,5 +106,7 @@ function Split-File (
 
 $currDir = "c:\sandbox\PowerShell"
 #entry point...
-Split-File -fileToSplit $currDir\JudeTheObscure.txt -outputRootDir $currDir -numberOfLinesPerFile 800
+#Split-File -splitMarker "SPLITHERE" -fileToSplit $currDir\PAndP.txt -outputRootDir $currDir
 
+#Split-File -fileToSplit C:\sandbox\PowerShell\SmallTestFile.txt -outputRootDir $currDir -numberOfLinesPerFile 5
+Split-File -fileToSplit C:\sandbox\PowerShell\JudeTheObscure.txt -outputRootDir $currDir -numberOfLinesPerFile 800
