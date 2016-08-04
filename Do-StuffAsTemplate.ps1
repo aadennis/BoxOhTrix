f17ee6439cf95cab8844b79494976c6e6fdb43d7
aadennis <dennis.circ@gmail.com>
2016-08-04 22:46:52 +0100

    Included Verb-Noun msdn link
    Included Verb-Noun msdn link

--- Do-StuffAsTemplate.ps1
+++ Do-StuffAsTemplate.ps1
@@ -11,7 +11,36 @@
 #>
 function Do-Things {
 [CmdletBinding(SupportsShouldProcess)]
-Param (
+Param (<# 
+       .Synopsis
+            Basic function template - replace with your own Things 
+       .Example 
+           Do-Things -ThingsPrefix "pre_"
+       .Example 
+           Do-Things -ThingsPrefix "pre_" -Verbose
+       .Example 
+           Do-Things -ThingsPrefix "before_" -LoopCount 3 -Verbose
+       #>
+       function Do-Things {
+       [CmdletBinding(SupportsShouldProcess)]
+       Param (
+       [Parameter(Mandatory)]
+           $ThingsPrefix,
+       [Parameter()]
+           [int] $LoopCount = 22
+       )
+           Begin {
+               Write-Verbose "I am in the Begin block with [$ThingsPrefix]"
+           }
+           Process {
+               1..$LoopCount | foreach {
+                   Write-Verbose "I am in the Process block [$_]"
+               }
+           }
+           End {
+               Write-Verbose "I am in the End block"
+           }
+       }
 [Parameter(Mandatory)]
     $ThingsPrefix,
 [Parameter()]
@@ -33,4 +62,6 @@ Param (
 
 New-ISESnippet -Text $m -Title "a Basic function template" -Description "Basic template to be enhanced." -Author "This Company" -Force
 
+#https://msdn.microsoft.com/en-us/library/ms714428(v=vs.85).aspx
+#Get-Verb
 
