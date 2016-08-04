b981ec4a465226f9a0f73accca0ffbb6e1a6d794
aadennis <dennis.circ@gmail.com>
2016-08-04 23:13:46 +0100

    Test-AllCharactersAllowed
    Test-AllCharactersAllowed

---
+++ Test-AllCharactersAllowed.ps1
@@ -0,0 +1,36 @@
+﻿<# 
+.Synopsis
+     If the passed string contains only allowed characters, return true, 
+     else return false.
+     The character is Allowed if it is not in the set of disallowed characters in the function
+.Example 
+    $actualResult = Test-AllCharactersAllowed $TestString
+#>
+function Test-AllCharactersAllowed {
+[CmdletBinding(SupportsShouldProcess)]
+Param (
+[Parameter(Mandatory)]
+    [string] $StringToCheck
+)
+   $DISALLOWED_CHARACTERS = '[",$% ]'
+   return $StringToCheck -notmatch $DISALLOWED_CHARACTERS
+}
+
+
+#Test function - not for production
+function Compare-ExpectedAndActualResults($TestString, $ExpectedResult) {
+    $actualResult = Test-AllCharactersAllowed $TestString
+    if ($ExpectedResult -ne $actualResult) {
+        Write-Host "FAIL: String under test:[$TestString];`t Expected:[$ExpectedResult];`t Actual: [$actualResult]" -BackgroundColor Red -ForegroundColor White
+    }
+     Write-Host "PASS: String under test:[$TestString];`t Expected:[$ExpectedResult];`t Actual: [$actualResult]" -BackgroundColor White -ForegroundColor Black
+}
+
+<# Tests...
+Compare-ExpectedAndActualResults "thisisok" $true
+Compare-ExpectedAndActualResults "thisis notok" $false
+Compare-ExpectedAndActualResults "also`$notok" $false
+Compare-ExpectedAndActualResults "ThisOneIs-Absolutely!Fine" $true
+#>
+
+
