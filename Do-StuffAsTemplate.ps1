d0026ca4c3730ec4c23bbca249a3745e1612f318
aadennis <dennis.circ@gmail.com>
2016-08-04 22:28:37 +0100

    basic function template for use in the ISE
    basic function template for use in the ISE

---
+++ Do-StuffAsTemplate.ps1
@@ -0,0 +1,36 @@
+﻿$m = @'
+<# 
+.Synopsis
+     Basic function template - replace with your own Things 
+.Example 
+    Do-Things -ThingsPrefix "pre_"
+.Example 
+    Do-Things -ThingsPrefix "pre_" -Verbose
+.Example 
+    Do-Things -ThingsPrefix "before_" -LoopCount 3 -Verbose
+#>
+function Do-Things {
+[CmdletBinding(SupportsShouldProcess)]
+Param (
+[Parameter(Mandatory)]
+    $ThingsPrefix,
+[Parameter()]
+    [int] $LoopCount = 22
+)
+    Begin {
+        Write-Verbose "I am in the Begin block with [$ThingsPrefix]"
+    }
+    Process {
+        1..$LoopCount | foreach {
+            Write-Verbose "I am in the Process block [$_]"
+        }
+    }
+    End {
+        Write-Verbose "I am in the End block"
+    }
+}
+'@
+
+New-ISESnippet -Text $m -Title "a Basic function template" -Description "Basic template to be enhanced." -Author "This Company" -Force
+
+
