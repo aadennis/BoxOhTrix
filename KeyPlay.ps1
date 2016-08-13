f3bf57d0c0e5586ea41e3fa95b49fd4448992c19
aadennis <dennis.circ@gmail.com>
2016-08-13 15:12:19 +0100

    use of HashTable in PowerShell
    use of HashTable in PowerShell

---
+++ KeyPlay.ps1
@@ -0,0 +1,18 @@
+﻿$myHashTable = @{
+    "Entry 1" = "President Clinton";
+    "Entry 2" = "Max Wall";
+    "Entry 3" = "Korky the Cat";
+}
+
+$myHashTable
+
+foreach ($entry in $myHashTable) {
+    "In the foreach: $entry"
+}
+
+foreach ($key in $myHashTable.Keys) {
+    "In the foreach: $key"
+    "The value: [$($myHashTable[$key])]"
+}
+
+
