e1cae8d235bc505e3b5888c9d43be83330b05e4e
aadennis <dennis.circ@gmail.com>
2016-09-11 17:55:23 +0100

    Create a VM(s) through the Azure CLI
    Create a VM(s) through the Azure CLI

---
+++ New-VmSet.ps1
@@ -0,0 +1,20 @@
+﻿#New-VmSet
+# Create a set of VMs using the Azure CLI
+
+$errorLog = "c:\temp\configErrors.log"
+$win2012R2Image = "a699494373c04fc0bc8f2bb1389d6106__Windows-Server-2012-R2-20160812-en.us-127GB.vhd"
+$username = "thomas"
+$password = "Pathword22!"
+$location = "`"East US`""
+$vmSet = "MahWeb01", "MahWeb02", "MahDb"
+
+
+$vmSet | ForEach-Object {
+    $vmName = $_
+    $vmCreateArgs = "vm create $vmName $win2012R2Image $username $password --location $location"
+    Write-Output "Arguments passed to azure.cmd: $vmCreateArgs"
+    Start-Process "azure.cmd" -ArgumentList $vmCreateArgs -NoNewWindow -RedirectStandardError $errorLog
+}
+
+
+
