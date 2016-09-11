b2a8a06483bfb0641b691d1cae20e7f48585d8d0
aadennis <dennis.circ@gmail.com>
2016-09-11 18:45:16 +0100

    Added the vm-size option
    Added the vm-size option

--- New-VmSet.ps1
+++ New-VmSet.ps1
@@ -6,15 +6,19 @@ $win2012R2Image = "a699494373c04fc0bc8f2bb1389d6106__Windows-Server-2012-R2-2016
 $username = "thomas"
 $password = "Pathword22!"
 $location = "`"East US`""
+$vmSize = "`"Standard_F4`""
 $vmSet = "MahWeb01", "MahWeb02", "MahDb"
 
 
 $vmSet | ForEach-Object {
     $vmName = $_
-    $vmCreateArgs = "vm create $vmName $win2012R2Image $username $password --location $location"
+    $vmCreateArgs = "vm create $vmName $win2012R2Image $username $password --location $location --vm-size $vmSize"
     Write-Output "Arguments passed to azure.cmd: $vmCreateArgs"
     Start-Process "azure.cmd" -ArgumentList $vmCreateArgs -NoNewWindow -RedirectStandardError $errorLog
 }
+
+Start-Sleep 10
+Start-Process "notepad.exe" -ArgumentList $errorLog
 
 
 
--- Remove-VmSet.ps1
+++ Remove-VmSet.ps1
@@ -6,7 +6,7 @@ $win2012R2Image = "a699494373c04fc0bc8f2bb1389d6106__Windows-Server-2012-R2-2016
 $username = "thomas"
 $password = "Pathword22!"
 $location = "`"East US`""
-$vmSet = "MahWeb01", "MahWeb02", "MahDb"
+$vmSet = "ClassicVM02", "ClassicVM05","ClassicVM06","ClassicVM04","ClassicVM03",
 
 $vmSet | ForEach-Object {
     $vmName = $_
