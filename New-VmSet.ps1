a1f7d03318c56423f26eb0983b12c7c4e352f14a
aadennis <dennis.circ@gmail.com>
2016-09-15 20:25:24 +0100

    various after struggling with Kaperskey blocking Git
    various after struggling with Kaperskey blocking Git

--- New-VmSet.ps1
+++ New-VmSet.ps1
@@ -2,22 +2,25 @@
 # Create a set of VMs using the Azure CLI
 
 $errorLog = "c:\temp\configErrors.log"
-$win2012R2Image = "a699494373c04fc0bc8f2bb1389d6106__Windows-Server-2012-R2-20160812-en.us-127GB.vhd"
+$win2012R2Image = "a699494373c04fc0bc8f2bb1389d6106__Windows-Server-2012-R2-20160812-en.us-127GB.vhd" 
+$sqlImage = "bd507d3a70934695bc2128e3e5a255ba__RightImage-Windows-2012-x64-sqlsvr2012ent-v14.2" # Windows 2012 (not R2), sqlsvr2012 
+$sqlImage2014 = "fb83b3509582419d99629ce476bcb5c8__SQL-Server-2014-SP1-12.0.4100.1-Std-ENU-Win2012R2-cy15su05" # Windows 2012 R2, sqlsvr2014 sp1
 $username = "thomas"
 $password = "Pathword22!"
 $location = "`"East US`""
 $vmSize = "`"Standard_F4`""
-$vmSet = "MahWeb01", "MahWeb02", "MahDb"
+$vmPrefix = "MahWeb2014x"
+$vmCount = 1
 
 
-$vmSet | ForEach-Object {
-    $vmName = $_
-    $vmCreateArgs = "vm create $vmName $win2012R2Image $username $password --location $location --vm-size $vmSize -r"
+1..$vmCount | ForEach-Object {
+    $vmName = $vmPrefix + $_
+    $vmCreateArgs = "vm create $vmName $sqlImage2014 $username $password --location $location --vm-size $vmSize -r"
     Write-Output "Arguments passed to azure.cmd: $vmCreateArgs"
     Start-Process "azure.cmd" -ArgumentList $vmCreateArgs -NoNewWindow -RedirectStandardError $errorLog
 }
 
-Start-Sleep 10
+Start-Sleep 60
 Start-Process "notepad.exe" -ArgumentList $errorLog
 
 
--- Remove-VmSet.ps1
+++ Remove-VmSet.ps1
@@ -1,12 +1,14 @@
 ﻿#Remove-VmSet
 # Delete a set of VMs using the Azure CLI
 
+Set-StrictMode -Version latest
+
 $errorLog = "c:\temp\removalErrors.log"
 $win2012R2Image = "a699494373c04fc0bc8f2bb1389d6106__Windows-Server-2012-R2-20160812-en.us-127GB.vhd"
 $username = "thomas"
 $password = "Pathword22!xx"
 $location = "`"East US`""
-$vmSet = "MahWeb01", "MahWeb02", "MahDb",
+$vmSet = "MahWebaxyz10"
 
 $vmSet | ForEach-Object {
     $vmName = $_
